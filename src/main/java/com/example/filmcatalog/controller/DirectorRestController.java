a8cae478edfa9de01885134007edfc8519be003b
Igor-Velicoborec <vel16035@mail,ru>
2021-02-07 12:50:31 +0300

    change method find. using @RequestParam

--- src/main/java/com/example/filmcatalog/controller/DirectorRestController.java
+++ src/main/java/com/example/filmcatalog/controller/DirectorRestController.java
@@ -6,7 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import com.example.filmcatalog.service.DirectorService;
-
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -58,12 +60,10 @@ public class DirectorRestController {
     }
 
 
-    @GetMapping(value = {"/name/{name}",
-            "/name/{name}/{birthDate}",
-            "/name/{name}/{birthDate}/{releaseDate}"})
-    public List<Director> getDirector(@PathVariable(required = false) String birthDate,
-                                      @PathVariable String name,
-                                      @PathVariable(required = false) String releaseDate) {
+    @GetMapping(value = "/find/")
+    public List<Director> find(@Valid @NotNull @Positive @RequestParam String name,
+                               @RequestParam(required = false)Date releaseFrom,
+                               @RequestParam(required = false) Date releaseTo) {
         List<Director> directors = null;
 
         return null;
